f61bf0205acf328123a74e04b0785d8399fb3d39
FlashA <hsalfdopi@gmail.com>
2015-09-20 18:36:00 +0300

    added activities for branches (fix)

--- app/src/main/java/com/flash/questionnaire/DesignActivity.java
+++ app/src/main/java/com/flash/questionnaire/DesignActivity.java
@@ -1,7 +1,16 @@
 package com.flash.questionnaire;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Anton on 20.09.2015.
  */
-public class DesignActivity {
+public class DesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design);
+    }
 }
--- app/src/main/java/com/flash/questionnaire/MainActivity.java
+++ app/src/main/java/com/flash/questionnaire/MainActivity.java
@@ -1,9 +1,11 @@
 package com.flash.questionnaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,25 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void func (View v) {
+        startActivity(new Intent(this, FuncActivity.class));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void design (View v) {
+        startActivity(new Intent(this, DesignActivity.class));
     }
+
+
 }
