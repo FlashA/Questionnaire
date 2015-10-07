445fb13875c08023441e9d717552f459667a769f
fridrikh <alnikf@mail.ru>
2015-10-07 16:41:27 +0300

    About

---
+++ app/src/main/java/com/flash/questionnaire/Utils/About.java
@@ -0,0 +1,23 @@
+package com.flash.questionnaire.Utils;
+
+/**
+ * Created by fdh on 07.10.15.
+ */
+public class About {
+    /*
+    Developers:
+
+        Maksimov Anton
+            Contants:
+                E-Mail: flashteso@gmail.com
+                Skype: flashkaa3
+                vk.com/the_flash_m
+
+
+        Fridrikh Alexandr
+            Contants:
+                E-Mail: alnikf@mail.ru
+                Skype: viksolo1
+                vk.com/id19328420
+    */
+}
